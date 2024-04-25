6cc7c8a8b9714cfc595648cd30f254b3493ab97b
Reihane Montazeri <rmontazeri82@gmail.com>
2024-04-24 20:13:28 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+﻿let $ = document
+let locationKey = $.querySelector('#location')
+let key = $.querySelector('#key')
+let whichkey = $.querySelector('#which')
+let code = $.querySelector('#code')
+let ascii = $.querySelector('#keyCode')
+
+let title = $.querySelector('title')
+
+document.body.addEventListener('keydown', function (event) {
+	
+	starter.style.display = 'none'
+	heading.style.display = 'flex'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+	console.log(event)
+	event.preventDefault()
+	ascii.innerHTML = event.keyCode
+	code.innerHTML = event.code
+	whichkey.innerHTML = event.which
+	key.innerHTML = event.key
+	locationKey.innerHTML = event.location
+	
+})
